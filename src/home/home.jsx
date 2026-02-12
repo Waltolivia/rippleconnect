e191bdfeaa83b095638cbb95bf9ecbb2c9f244e1
WaltOlivia <livwalton1@gmail.com>
2026-02-12 16:10:00 -0700

    Home and notes jsx conversion

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React from "react";
 
 export function Home() {
   return (
     <main>
-      <h1>Home Page</h1>
+      <div className="page-content">
+
+        <div className="notebook_bar">
+          <ul>
+            <li>
+              <a href="#" className="bar-item-button">Notebook1</a>
+            </li>
+            <li>
+              <a href="#" className="bar-item-button">Notebook2</a>
+            </li>
+            <li>
+              <a href="#" className="bar-item-button">Notebook3</a>
+            </li>
+          </ul>
+
+          <button className="new_notebook" type="button">
+            New Notebook
+          </button>
+        </div>
+
+        <div className="content">
+          <h1>Home</h1>
+          <h3>
+            This is a place to make connections, let ideas flow, and let colors help you learn or write what you need to!
+          </h3>
+
+          <img src="/images/download.png" alt="Blue Links" />
+
+          <h2>Start a New Note</h2>
+          <button className="button_start">Start Here</button>
+
+          <p>
+            Notebooks are the home to your ideas and connections. Start one and use the notes,
+            pages, and sticky notes inside to help you write!
+          </p>
+
+          <div className="Login">
+            <form>
+              <label htmlFor="username">Username </label>
+              <input type="text" id="username" name="username" />
+              <br /><br />
+
+              <label htmlFor="password">Password</label>
+              <input type="text" id="password" name="password" />
+              <br /><br />
+            </form>
+          </div>
+
+        </div>
+      </div>
     </main>
   );
 }
--- src/notes/notes.jsx
+++ src/notes/notes.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React from "react";
 
 export function Notes() {
   return (
     <main>
-      <h1>Notes Page</h1>
+      <div className="page-content">
+        <div className="notebook_bar">
+          <ul>
+            <li>
+              <a href="#" className="bar-item-button">Notebook1</a>
+            </li>
+            <li>
+              <a href="#" className="bar-item-button">Notebook2</a>
+            </li>
+            <li>
+              <a href="#" className="bar-item-button">Notebook3</a>
+            </li>
+          </ul>
+
+          <button className="new_notebook" type="button">
+            New Notebook
+          </button>
+        </div>
+
+        <div className="content-notes">
+          <div className="note">
+            <h1>Notebook1 - Page1</h1>
+            <h3>This is one of the Notebook pages.</h3>
+            <p>Paragraph text...</p>
+            <p>More paragraph text...</p>
+          </div>
+
+          <div className="stickynotes">
+            <div className="sticky-note">
+              This is a small sticky note to the side.
+            </div>
+
+            <div className="index-card">
+              <img
+                src="/images/starryexampleimg.png"
+                alt="Blue Links"
+                width="250"
+              />
+              <p>This is the starry night painting</p>
+
+              <ul>
+                <li>It is a beautiful painting</li>
+                <li>I love the colors</li>
+                <li>This is a note for ideas</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="New-Note">
+            <form>
+              <label htmlFor="title">Title: </label>
+              <input type="text" id="title" name="title" />
+              <br /><br />
+
+              <label htmlFor="note">Text:</label>
+              <input type="text" id="note" name="note" />
+              <br /><br />
+            </form>
+          </div>
+
+        </div>
+      </div>
     </main>
   );
 }
